ae91f004f459341585aad49d75548422d875d2a4
viih96 <vinicius3439@gmail.com>
2021-04-15 01:09:09 -0300

    added cadastro login

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -2,6 +2,8 @@ import { Login } from './../users/shared/login';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { UsersAgentesaude } from '../users/shared/users-agentesaude';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ export class AuthService {
 
   constructor(
     private afa: AngularFireAuth,
+    private afs: AngularFirestore,
     private router: Router) { }
 
   login(Login: Login){
@@ -21,4 +24,36 @@ export class AuthService {
     this.afa.signOut();
     this.router.navigate(['/login']);
   }
+
+  registerAgente(user: UsersAgentesaude){
+    this.afa.createUserWithEmailAndPassword(user.email, user.password);
+
+    this.afa.onAuthStateChanged((userProfile)=>{
+      userProfile.updateProfile({displayName: user.name, photoURL: ''});
+      if(userProfile){
+        this.registerAgentesaude(user, userProfile.uid);
+      }
+    })
+  }
+
+  registerAgentesaude(user: UsersAgentesaude, id: string){
+        const { name,
+          email,
+          registro,
+          address_state,
+          id_professional,
+          professional,   } = user;
+    this.afs.collection('users').doc(id).set(
+      {
+        name: name,
+        email: email,
+        registro: registro,
+        address_state: address_state,
+        id_professional: id_professional,
+        professional: professional,
+      }
+    )
+  }
+
 }
+
---
+++ src/app/signup/signup.page.ts
@@ -0,0 +1,70 @@
+import { ProfessionalService } from './../shared/professional.service';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastService } from '../shared/toast.service';
+import { Router } from '@angular/router';
+import { UsersAgentesaude } from '../users/shared/users-agentesaude';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+})
+export class SignupPage implements OnInit {
+  usersAgentesaude: UsersAgentesaude;
+  tipousuario: string;
+  email: string;
+  password: string;
+  user: string;
+  professionals: any[] = [];
+
+  professionalDescription: string;
+
+  constructor(private auth:AuthService,
+    private professionalService: ProfessionalService,
+    private afa: AngularFireAuth,
+    // private cepService: CepService,
+    // private validaCPFService: ValidaCpfService,
+    private toast: ToastService,
+    private router: Router) { }
+
+    ngOnInit() {
+      this.usersAgentesaude = new UsersAgentesaude();
+      this.professionalService.getAll().subscribe( (data:any) => {
+        this.professionals = data;
+      });
+    }
+
+    setProfessional(professional:any){
+      if(professional){
+        // console.log(professional);
+        this.usersAgentesaude.professional = this.professionals.find(item => item.id === this.usersAgentesaude.id_professional).description
+      } else {
+        this.usersAgentesaude.professional = "";
+      }
+  }
+
+  getProfessional(id: string){
+    const subscribe = this.professionalService.getProfessional(id).subscribe( (data: any) =>{
+      subscribe.unsubscribe();
+      this.usersAgentesaude.professional = data.description;
+    })
+  }
+
+  async registerAgentesaude(){
+    this.usersAgentesaude.email = this.email;
+    this.usersAgentesaude.password = this.password;
+    // this.usersAgentesaude.tipousuario = this.tipousuario;
+    // this.getProfessional(this.usersAgentesaude.id_professional);
+
+    try {
+      await this.auth.registerAgente(this.usersAgentesaude);
+      this.toast.showMessageBottom('Usuário registrado com sucesso !!!', 'secondary');
+      this.router.navigate(['login']);
+    } catch (error) {
+      this.toast.showMessageTop(error,'danger');
+    }
+  }
+
+}
