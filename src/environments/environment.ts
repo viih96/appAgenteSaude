aa0fe9d7669f8332f0a27500e5b0a9fdfdf399c2
Ivan Sena <ivanprojetosti@gmail.com>
2021-05-05 20:06:01 -0300

    05/05 environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import { firebaseConfig } from './../firebaseConfig';
-=======
 import { firebaseConfig } from "./../firebaseConfig";
->>>>>>> 2b6d0cc9acd83095dd0cef99f80833a7afc35522
+
 
 export const environment = {
   production: false,
